a6af073c93cb9f84ad313512516d33c46737436b
Bryan Pedraza <Bryanpredrazay57@icloud.com>
2024-03-01 22:32:01 -0800

    Terra-aws

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "terra-ec2" {
+  ami = "ami-05fa00d4c63e32376"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "TF-Instance"
+  }
+}
+
+//Bk_o7M9-
+
+
+
+
